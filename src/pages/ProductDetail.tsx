30475f8bae5780baa43e9562c3e67b9fda9e1c58
riccardo-ottavi <ottavi.ps@libero.it>
2026-07-29 01:26:43 +0200

    feat: display product details infos

--- src/pages/ProductDetail.tsx
+++ src/pages/ProductDetail.tsx
@@ -1,5 +1,37 @@
-export default function ProductDetail(){
-    return(
-        <p>ProductDetail</p>
+import { useParams } from "react-router-dom"
+import { useState, useEffect } from "react";
+import type { Product } from "../types/product";
+import { fetchProductById } from "../api/product";
+
+export default function ProductDetail() {
+
+    const { id } = useParams();
+
+    const [product, setProduct] = useState<Product | null>(null);
+
+    useEffect(() => {
+
+        async function loadProduct() {
+            if (!id) return;
+            const data = await fetchProductById(id);
+            setProduct(data);
+        }
+
+        loadProduct();
+
+    }, [id]);
+
+    return (
+        <div className="details">
+            <img src={product?.image} alt="" />
+            <div className="details-text">
+                <span>{product?.category}</span>
+                <span>{product?.title}</span>
+                <p>{product?.description}</p>
+                <span>{product?.price.toFixed(2)}€</span>
+                <span>{product?.rating.rate}</span>
+                <span>{product?.rating.count}</span>            
+            </div>
+        </div>
     )
 }
