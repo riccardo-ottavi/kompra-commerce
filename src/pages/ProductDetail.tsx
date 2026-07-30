dfbb21657a433144afbba195cc9478d6f563e4a9
riccardo-ottavi <ottavi.ps@libero.it>
2026-07-30 18:05:28 +0200

    feat: add add-to-cart button to product detail

--- src/pages/ProductDetail.tsx
+++ src/pages/ProductDetail.tsx
@@ -3,10 +3,14 @@ import { useState, useEffect } from "react";
 import type { Product } from "../types/product";
 import { fetchProductById } from "../api/product";
 import Rating from "../components/Rating";
+import cartIcon from "../assets/cart-icon.svg";
+import { useCart } from "../contexts/CartContext";
 
 export default function ProductDetail() {
 
     const { id } = useParams();
+
+    const { addToCart } = useCart();
 
     const [product, setProduct] = useState<Product | null>(null);
 
@@ -36,6 +40,12 @@ export default function ProductDetail() {
                             rate={product.rating.rate}
                             count={product.rating.count}
                         />
+                    </div>
+                    <div 
+                        className="cart-btn"
+                        onClick={()=> addToCart(product)}
+                    >
+                        <img src={cartIcon} alt="cart-icon" className="cart-icon" />
                     </div>
                 </div>
             )}
